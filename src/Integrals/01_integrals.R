c9b2474e4b5e59734876fe8d8004b2d84bf306d6
Ales Cervenka <ales.cervenka@inzata.com>
2017-07-18 17:55:57 +0200

    FILE: Completed the integration examples.

--- src/Integrals/01_integrals.R
+++ src/Integrals/01_integrals.R
@@ -5,93 +5,40 @@
 # (preferebly by checking Ryacas on the Packages tab under "User Library")
 # library("Ryacas", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.4")
 
-
-S 0 dx = C
-S dx = x + C
-S x^n dx = x^(n+1) / (n + 1)
-S 1/x dx = ln |x| + c
-S e^x dx = e^x + C
-S a^x dx = a^x/ln a + C for a > 0
-S sin x dx = - cos x + C
-S cos x dx = sin x + C
-
-
-
 #-----------------------------------------------------------------------------
 # Constant function
 # -----------------
 
-D(expression(5), "x")
-D(expression(6), "x")
+yacas("Integrate(x) 0")
 
 # Linear function
 # ---------------
 
-D(expression(5 * x), "x")
-D(expression(6 * x), "x")
-
-# do the same with Ryacas
-xx <- Sym("xx")
-yacas(deriv(6*xx, xx))
+yacas("Integrate(x) 1")
+yacas("Integrate(x) 2")
 
 # Polynomial functions
 # --------------------
 
-D(expression(x^2), "x")
-D(expression(x^3), "x")
-D(expression(x^4), "x")
-D(expression(x^4 + x^3 + x^2), "x")
-yacas(deriv(xx^4 + xx^3 + xx^2, xx))
+yacas("Integrate(x) x^3")
 
-# Trigonometric functions
-# -----------------------
+# Rational functions
+# ------------------
 
-D(expression(sin(x)), "x")
-D(expression(cos(x)), "x")
-D(expression(tan(x)), "x")
-D(expression(1/tan(x)), "x") # returns -(1/cos(x)^2/tan(x)^2), which is 
-                             # equivalent to -(1/sin(x)^2)
-
-yacas(deriv(sin(xx), xx))
-yacas(deriv(cos(xx), xx))
-yacas(deriv(tan(xx), xx))
-yacas(deriv(1/tan(xx), xx))
+yacas("Integrate(x) 1/x")
 
 # Exponential functions
 # ---------------------
 
-D(expression(3^x), "x")
-D(expression(4^x), "x")
-D(expression(5^x), "x")
+yacas("Integrate(x) a^x")
+yacas("Integrate(x) e^x")
 
-yacas(deriv(5^xx, xx))
+# Trigonometric functions
+# -----------------------
 
-# Logarithmic functions
-# ---------------------
+yacas("Integrate(x) Sin(x)")
+yacas("Integrate(x) Cos(x)")
 
-# derivative of y = ln(x)
-yacas("D(x)Ln(x)")
-# derivative of y = log(x, 5) = ln(x) / ln(5)
-yacas("D(x)(Ln(x)/Ln(5))")
 
-# Plotting a derivative function
-# ------------------------------
-
-curve(sin(x), -8, 8, col = "tan4", ylim = c(-2, 2))
-curve(eval(D(expression(sin(x)), "x")), -8, 8, add = TRUE, col = "blue1")
-abline(h = 0, v = 0)
-
-# now do the same with Ryacas
-
-curve(sin(x), -8, 8, col = "tan4", ylim = c(-2, 2))
-xx <- Sym("xx")
-my_deriv <- yacas(deriv(sin(xx), xx))
-my_deriv2 <- function(xx) {
-  eval(parse(text = my_deriv$text))
-}
-curve(my_deriv2(x), -8, 8, add = TRUE, col = "blue1")
-abline(h = 0, v = 0)
-
-rm(list=ls())
 
 
